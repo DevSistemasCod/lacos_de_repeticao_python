de88b91ef95252a76498148c8afb72715504b670
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2024-02-02 14:40:52 -0300

    Add files via upload

---
+++ exerc_07.py
@@ -0,0 +1,13 @@
+dividendo = int(input("Informe o primeiro número: "))
+
+divisor = 2
+resto = 0
+
+while dividendo > 1:
+    resto = dividendo % divisor
+    
+    if resto == 0:
+        print(divisor)
+        dividendo =  dividendo // divisor
+    else:
+        divisor = divisor + 1
